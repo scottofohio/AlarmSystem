b90778d11e8c4d4609778feea1925d19be68c3cb
Scott <scottbakers@gmail.com>
2021-03-18 10:01:31 -0400

    kept keypad code if someone wants it but I don't think it's needed if we are running everything on a local network, it will make it faster

--- WebInterface/assets/js/scripts.js
+++ WebInterface/assets/js/scripts.js
@@ -1,7 +1,7 @@
 $ = jQuery;
 $(document).ready(function () {
   // Get Url Paramaters 
-  var url = new URL(window.location.href + '?keycode=false');
+  var url = new URL(window.location.origin + '?keycode=false');
   var params = new URLSearchParams(url.search);
   var keyPad = $('#key');
   var inputKeys = [];
@@ -15,13 +15,29 @@ $(document).ready(function () {
     }
   });
   $('#snd').on('click', function () {
-    params.set('keycode', keyPad);
+    params.set('keycode', keyPad.val());
     url.search = params.toString();
     if (keyPad.val() == '3045') {
       window.location.href = url;
+      console.log(url);
     } else {
       keyPad.addClass('error');
       keyPad.val('Incorrect Pin');
+      inputKeys = [];
+      x = 1;
+      setInterval(function () {
+         if (x == 4) { 
+             window.location.href = window.location.origin + '?keycode=wrong-pin';
+         } else {
+            keyPad.val(x);
+             x++;
+         }
+     }, 1000);      
     }
+  });
+  $('#clr').on('click',function(){
+    keyPad.val('');
+    inputKeys = [];
+    window.location.href = window.location.origin;
   });
 });
